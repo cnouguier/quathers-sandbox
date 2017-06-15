ab50b9415b226adda5a02bf0af66ac465ac401ce
Christophe Nouguier <christophe.nouguier@free.fr>
2017-06-16 01:12:15 +0200

    Add a Dialog ton confirm the deletion

--- quathers/src/mixins/collection/mixin.delete-item.js
+++ quathers/src/mixins/collection/mixin.delete-item.js
@@ -1,9 +1,22 @@
 import mixinStore from '../mixin-store'
+import { Dialog } from 'quasar'
 
 let deleteItemMixin = {
   methods: {
     deleteItem (service, item) {
-      console.log(service + ': deleteItem from mixin')
+      Dialog.create({
+        title: 'Warning',
+        message: 'Are you sure you want to delete ' + item.name + '?',
+        buttons: [
+          'Cancel',
+          {
+            label: 'Delete',
+            handler () {
+              console.log('Not implemented')
+            }
+          }
+        ]
+      })
     }
   },
   mounted () {
