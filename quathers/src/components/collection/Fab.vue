5e9722ca6799184632b88c49c4d731b1288edb3f
Christophe Nouguier <christophe.nouguier@free.fr>
2017-06-16 07:27:13 +0200

    Enhance collection configuration

--- quathers/src/components/collection/Fab.vue
+++ quathers/src/components/collection/Fab.vue
@@ -4,8 +4,7 @@
       Render an expandable fab if multiple actions are provided
      -->
     <q-fab v-if="actions.length > 1"
-      class="absolute-bottom-right primary"
-      style="right: 24px; bottom: 24px"
+      class="absolute-bottom-right primary fab-margin"
       icon="keyboard_arrow_up"
       direction="up">
         <q-small-fab v-for="action in actions" :key="action.id"
@@ -18,8 +17,7 @@
       Render a non expandable fab if a single action is provided
      -->
     <button v-else-if="actions.length === 1"
-      class="absolute-bottom-right primary circular"
-      style="right: 24px; bottom: 24px"
+      class="absolute-bottom-right primary circular fab-margin"
       @click="$emit('actionTrigerred', actions[0].handler)">
       <i>{{ actions[0].icon }}</i>
     </button>
@@ -43,3 +41,9 @@ export default {
   }
 }
 </script>
+
+<style lang="styl">
+.fab-margin
+  right 24px 
+  bottom 24px
+</style>
